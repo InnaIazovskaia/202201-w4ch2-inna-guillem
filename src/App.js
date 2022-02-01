8341a7e57d8688b855e7d44a662b367b9f098138
GuillemXanxo <jscodebaker@gmail.com>
2022-02-01 20:06:16 +0100

    Added Letra loop in app.js

--- src/App.js
+++ src/App.js
@@ -36,102 +36,12 @@ function App() {
       </section>
       <section class="game-result">You're dead!</section>
       <ul class="letters">
-        {}
-        {gentlemenInfo.map((gentleman) => {
+        {abecedario.map((letra) => {
           return (
-            <Gentlemen
-              key={gentleman.id}
-              name={gentleman.name}
-              status={gentleman.status}
-              profession={gentleman.profession}
-              twitter={gentleman.twitter}
-              picture={gentleman.picture}
-              alternativeText={gentleman.alternativeText}
-            />
+            <Letra id={letra.id} letra={letra.letra} elegida={letra.elegida} />
           );
         })}
-
-        <li class="letter">
-          <a href="a">A</a>
-        </li>
-        <li class="letter">
-          <a href="b">B</a>
-        </li>
-        <li class="letter">
-          <a href="c">C</a>
-        </li>
-        <li class="letter">
-          <a href="d">D</a>
-        </li>
-        <li class="letter">
-          <a href="e">E</a>
-        </li>
-        <li class="letter">
-          <a href="f">F</a>
-        </li>
-        <li class="letter">
-          <a href="g">G</a>
-        </li>
-        <li class="letter">
-          <a href="h">H</a>
-        </li>
-        <li class="letter">
-          <a href="i">I</a>
-        </li>
-        <li class="letter">
-          <a href="j">J</a>
-        </li>
-        <li class="letter">
-          <a href="k">K</a>
-        </li>
-        <li class="letter">
-          <a href="l">L</a>
-        </li>
-        <li class="letter">
-          <a href="m">M</a>
-        </li>
-        <li class="letter">
-          <a href="n">N</a>
-        </li>
-        <li class="letter">
-          <a href="ñ">Ñ</a>
-        </li>
-        <li class="letter">
-          <a href="o">O</a>
-        </li>
-        <li class="letter">
-          <a href="p">P</a>
-        </li>
-        <li class="letter">
-          <a href="q">Q</a>
-        </li>
-        <li class="letter">
-          <a href="r">R</a>
-        </li>
-        <li class="letter">
-          <a href="s">S</a>
-        </li>
-        <li class="letter">
-          <a href="t">T</a>
-        </li>
-        <li class="letter">
-          <a href="u">U</a>
-        </li>
-        <li class="letter">
-          <a href="v">V</a>
-        </li>
-        <li class="letter">
-          <a href="w">W</a>
-        </li>
-        <li class="letter">
-          <a href="x">X</a>
-        </li>
-        <li class="letter">
-          <a href="y">Y</a>
-        </li>
-        <li class="letter">
-          <a href="z">Z</a>
-        </li>
+        ;
       </ul>
     </>
   );
